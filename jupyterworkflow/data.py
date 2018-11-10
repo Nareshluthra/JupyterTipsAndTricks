9f2ba00aee2ccbdf85ea18f88fa7195d14f27eda
Naresh Luthra <nareshluthra2003@yahoo.co.in>
2018-11-10 22:49:55 +0530

    Edited data upload

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 FREEMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
 
-def get_fremont_data(filename='Fremont.csv' , url=FREEMONT_URL ,
+def get_fremont_data(filename='Freemont.csv' , url=FREEMONT_URL ,
              force_download=False):
 
     """Download and cache the freemont data
@@ -30,7 +30,12 @@ def get_fremont_data(filename='Fremont.csv' , url=FREEMONT_URL ,
     if force_download or not os.path.exists(filename):
     
             urlretrieve(url,filename)
-    data = pd.read_csv('Freemont.csv',index_col='Date', parse_dates=True)
+    data = pd.read_csv('Freemont.csv',index_col='Date')
+
+    try:
+    	data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+    except TypeError:
+    	data.index = pd.to_datetime(data.index)
     data.columns =['East','West']
     data['Total'] =data['West'] + data['East']
     return data
